2db91ee9ce194490902c9bf8a10befcff1ccf0a6
Samer Madieh <smadieh@sdsu.edu>
2021-04-23 17:59:44 -0700

    Added project files to repo

---
+++ Assets/Scripts/flock.cs
@@ -0,0 +1,101 @@
+﻿// Name: Samer Madieh
+// File: flock.cs
+// Description: script for single boid movement. This script is attached to the boid prefab.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour
+{
+    public float speed = 0.001f;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePostion;
+    float neighborDistance = 3.0f;
+
+    bool turn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, Vector3.zero) >= globalFlock.tankSize)
+        {
+            turn = true;
+        }
+        else
+        {
+            turn = false;
+        }
+
+        if(turn == true)
+        {
+            Vector3 direction = Vector3.zero - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            speed = Random.Range(1, 1.5f);
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+            {
+                ApplyRules();
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    // Apply Rules to boids function
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = globalFlock.boids;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float groupSpeed = 0.1f;
+
+        Vector3 goal = globalFlock.goal;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if(dist <= neighborDistance)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+
+                    if(dist < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    flock anotherFlock = go.GetComponent<flock>();
+                    groupSpeed = groupSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0)
+        {
+            vcenter = vcenter / groupSize + (goal - this.transform.position);
+            speed = groupSpeed / groupSize;
+
+            Vector3 direction = (vcenter + vavoid) - transform.position;
+            if(direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/globalFlock.cs
@@ -0,0 +1,112 @@
+﻿// Name: Samer Madieh
+// File: globalFlock.cs
+// Description: script for the whole flock and the BoidManager object. It also controls the UI elements and provides functions for them.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class globalFlock : MonoBehaviour
+{
+    public GameObject boidPrefab;
+    public GameObject goalPrefab;
+    public static int tankSize = 5;
+    public Button lazyBtn;
+    public Button circleBtn;
+
+    int isRunning = 1;
+    int numSeconds = 4;
+    bool lazy = true;
+
+    static int numBoids = 20;
+    public static GameObject[] boids = new GameObject[numBoids];
+    Coroutine co;
+
+    public static Vector3 goal = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numBoids; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize), Random.Range(-tankSize, tankSize), Random.Range(-tankSize, tankSize));
+            boids[i] = (GameObject)Instantiate(boidPrefab, pos, Quaternion.identity);
+            lazyBtn.onClick.AddListener(lazyClicked);
+            circleBtn.onClick.AddListener(userClicked);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check Which Mode is active
+        if(lazy)
+        {
+            if (isRunning == 1)
+            {
+                co = StartCoroutine(LazyWait());
+            }
+        }
+        else
+        {
+            // Check if buttons are pressed to move target
+            if (goalPrefab.transform.position.y < 5)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    goalPrefab.transform.Translate(Vector3.up * Time.deltaTime * 2.5f);
+                    goal = goalPrefab.transform.position;
+                }
+            }
+            if (goalPrefab.transform.position.y > -5)
+            {
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    goalPrefab.transform.Translate(Vector3.down * Time.deltaTime * 2.5f);
+                    goal = goalPrefab.transform.position;
+                }
+            }
+            if (goalPrefab.transform.position.x < 5)
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    goalPrefab.transform.Translate(Vector3.right * Time.deltaTime * 2.5f);
+                    goal = goalPrefab.transform.position;
+                }
+            }
+            if (goalPrefab.transform.position.x > -5)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    goalPrefab.transform.Translate(Vector3.left * Time.deltaTime * 2.5f);
+                    goal = goalPrefab.transform.position;
+                }
+            }
+        }
+    }
+
+    // Function for Lazy target position switching
+    IEnumerator LazyWait()
+    {
+        isRunning = 0;
+        yield return new WaitForSeconds(numSeconds);
+        goal = new Vector3(Random.Range(-tankSize, tankSize), Random.Range(-tankSize, tankSize), Random.Range(-tankSize, tankSize));
+        goalPrefab.transform.position = goal;
+        isRunning = 1;
+    }
+
+    // Functions for UI buttons clicked
+    void lazyClicked()
+    {
+        lazy = true;
+    }
+
+    void userClicked()
+    {
+        lazy = false;
+        StopCoroutine(co);
+        goal = new Vector3(0, 0, 0);
+        goalPrefab.transform.position = goal;
+    }
+}
